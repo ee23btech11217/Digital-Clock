9792063d733176f70f62087763568202d5b9aedd
Prajwal M <ee23btech11217@iith.ac.in>
2024-05-03 02:15:14 +0530

    formmattime bug fixed

--- codes/features.v
+++ codes/features.v
@@ -120,10 +120,9 @@ module setdate (
     output reg [3:0] month2,
     output reg [3:0] year1,
     output reg [3:0] year2, 
-    output reg [2:0] day
-);
-
-//when set_mode = 3, 
+    output reg [2:0] day );
+    
+    //when set_mode = 3, 
 
     parameter DAY = 2'b00, MONTH = 2'b01, YEAR = 2'b10, WEEKDAY = 2'b11;
     reg [1:0] state;
@@ -210,71 +209,72 @@ module settime (
     output reg [3:0] min1,
     output reg [3:0] min2,
     output reg [3:0] sec1,
-    output reg [3:0] sec2
-);
+    output reg [3:0] sec2 );
 
     parameter HOUR = 2'b00, MIN = 2'b01, SEC = 2'b10, DONE = 2'b11;
 
     reg [1:0] state;
-    reg issetpressednow;
+    reg issetpressednow = 0;
 
     always @(posedge clk) begin
         if (set_mode == 2'b01) begin
-            if (issetpressednow) begin
-                state <= 0;
-                hour1 <= 0;
-                hour2 <= 0;
-                min1 <= 0;
-                min2 <= 0;
-                sec1 <= 0;
-                sec2 <= 0;
-                issetpressednow <= 0;
+            if (!issetpressednow) begin // Check if setting mode is just entered
+                state <= HOUR;
+                issetpressednow <= 1; // Reset issetpressednow flag
+                hour1 <= 4'b0;
+                hour2 <= 4'b0;
+                min1 <= 4'b0;
+                min2 <= 4'b0;
+                sec1 <= 4'b0;
+                sec2 <= 4'b0;
             end
+            else begin
+                case(state)
+                    HOUR: begin
+                        if (button1) begin
+                            hour1 <= (hour1 == 4'h2) ? 4'b0 : hour1 + 1;
+                        end
+                        if (button2) begin
+                            hour2 <= (hour2 == 4'h9) ? 4'b0 : hour2 + 1;
+                        end
+                        if (button3) begin
+                            state <= MIN;
+                        end
+                    end
 
-            casex(state)
-                HOUR: begin
-                    if (button1) begin
-                        hour1 <= (hour1 == 4'h2) ? 4'b0 : hour1 + 1;
+                    MIN: begin
+                        if (button1) begin
+                            min1 <= (min1 == 4'h5) ? 4'b0 : min1 + 1;
+                        end
+                        if (button2) begin
+                            min2 <= (min2 == 4'h9) ? 4'b0 : min2 + 1;
+                        end
+                        if (button3) begin
+                            state <= SEC;
+                        end
                     end
-                    if (button2) begin
-                        hour2 <= (hour2 == 4'h9) ? 4'b0 : hour2 + 1;
-                    end
-                    if (button3) begin
-                        state <= MIN;
-                    end
-                end
 
-                MIN: begin
-                    if (button1) begin
-                        min1 <= (min1 == 4'h5) ? 4'b0 : min1 + 1;
+                    SEC: begin
+                        if (button1) begin
+                            sec1 <= (sec1 == 4'h5) ? 4'b0 : sec1 + 1;
+                        end
+                        if (button2) begin
+                            sec2 <= (sec2 == 4'h9) ? 4'b0 : sec2 + 1;
+                        end
+                        if (button3) begin
+                            state <= HOUR;
+                        end
                     end
-                    if (button2) begin
-                        min2 <= (min2 == 4'h9) ? 4'b0 : min2 + 1;
-                    end
-                    if (button3) begin
-                        state <= SEC;
-                    end
-                end
-
-                SEC: begin
-                    if (button1) begin
-                        sec1 <= (sec1 == 4'h5) ? 4'b0 : sec1 + 1;
-                    end
-                    if (button2) begin
-                        sec2 <= (sec2 == 4'h9) ? 4'b0 : sec2 + 1;
-                    end
-                    if (button3) begin
-                        state <= HOUR;
-                    end
-                end
-                default: state <= HOUR;
-            endcase
+                    default: state <= HOUR;
+                endcase
+            end
         end
-        else begin
-           issetpressednow <= 1;
+        else if (set_mode == 2'b00) begin
+            issetpressednow <= 0; // Reset issetpressednow flag when setting mode ends
         end
     end
 endmodule //settime
+
 
 module clocktime(input wire clk_1hz, input wire rst, input wire[1:0] clk_mode, input wire [23:0] time_in, output wire[23:0] time_out);
 
@@ -365,42 +365,26 @@ module clocktime(input wire clk_1hz, input wire rst, input wire[1:0] clk_mode, i
 endmodule //clocktime
 
 module formattime (
-    input wire [1:0] clk_mode,     // Clock mode (2-bit)
-    input wire setampm,  // Time format toggle signal
-    input wire [23:0] clock_time, // Current clock time (24-bit)
-    input wire [23:0] alarm_time, // Alarm time (24-bit)
-    output reg [23:0] bcd_time,   // Converted time output (24-bit)
-    output reg ampm               // AM/PM flag output
-);
+  input wire [1:0] clk_mode,     // Clock mode (2-bit)
+  input wire setampm,  // Time format toggle signal
+  input wire [23:0] clock_time, // Current clock time (24-bit)
+  input wire [23:0] alarm_time, // Alarm time (24-bit)
+  output [23:0] bcd_time,   // Converted time output (24-bit)
+  output ampm);
 
-// Temporary storage for time based on clock mode
-reg [23:0] bcd_time_temp;
-reg formattimetoggle;
+  wire [5:0] hour24Sub;
+  wire [23:0] hour24; 
+  wire [5:0] hour12;
+  wire issetampm = 0;
 
-// Select clock or alarm time based on clock mode
-always @* begin
-    if (clk_mode == 2'b10)
-        bcd_time_temp = alarm_time;
-    else bcd_time_temp = clock_time;
+  assign hour24 = (clk_mode == 2'b11) ? alarm_time : clock_time;
+  assign hour24Sub = hour24 - ((hour24[21:17]==5'b10000) ? 6'h18 : 6'h12); //takes care of the 9,10 bug in pm mode
+  
+  assign ampm = (hour24[23:16] > 8'h11); //1-->pm, 0-->am
+  assign hour12 = ((hour24 == 24'h0) | (hour24 == 24'h120000)) ? 6'h12 : ((ampm) ? hour24Sub : hour24[21:16]);
 
-    if (setampm == 1) begin
-        // Toggle time format
-        formattimetoggle = ~formattimetoggle;
-    end
-
-    if (formattimetoggle) // 24-hour format
-        bcd_time = bcd_time_temp;
-    else begin // 12-hour format
-        if (bcd_time_temp[23:16] > 8'h12) begin // PM
-            bcd_time[23:16] = bcd_time_temp[23:16] - 8'h12;
-            ampm = 1; // Set PM flag
-        end else begin // AM
-            bcd_time[23:16] = bcd_time_temp[23:16];
-            ampm = 0; // Set AM flag
-        end
-        bcd_time[15:0] = bcd_time_temp[15:0]; // Keep lower 16 bits unchanged
-    end
-end
+  assign issetampm = (setampm == 1) ? ~issetampm : issetampm;
+  assign bcd_time = (issetampm) ? {2'b00, hour12, hour24[15:0]} : hour24;
 
 endmodule //formattime
 
