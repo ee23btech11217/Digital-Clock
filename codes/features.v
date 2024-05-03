26275132be949517cec0c6bb28a35517becc772e
Prajwal M <ee23btech11217@iith.ac.in>
2024-05-03 10:15:12 +0530

    timer

--- codes/features.v
+++ codes/features.v
@@ -1,3 +1,85 @@
+module timer(input clk_1hz, input [1:0] timer_mode, input [23:0] time_in, output [23:0] time_out, output buzzer);
+
+  //separated time signals to respective meaning
+  wire [7:0] sec_in, min_in, hour_in;
+  reg [7:0] sec_reg, min_reg, hour_reg;
+
+  //separation and combination of time signals
+  assign {hour_in, min_in, sec_in} = time_in;
+  assign time_out = {hour_reg, min_reg, sec_reg};
+  assign buzzer = (time_out == 24'h0) ? 1 : 0; //how many seconds?
+
+    always@(posedge clk_1hz)
+    begin 
+    //shallisetnow is one for 
+    if (timer_mode == 2'b01)
+        begin
+          sec_reg <= sec_in;
+          min_reg <= min_in;
+          hour_reg <= hour_in;
+        end
+    else begin
+            //secs
+          casex(sec_reg)
+            8'h00:
+              begin
+                sec_reg <= 8'h59;
+              end
+            8'h?0:
+              begin
+                sec_reg <= {(sec_reg[7:4]-4'h1), 4'h9};
+              end
+            default:
+              begin
+                sec_reg <= sec_reg - 8'h01;
+              end
+          endcase
+
+            //mins
+          if(sec_reg == 8'h00)
+            begin
+              casex(min_reg)
+                8'h00:
+                  begin
+                    min_reg <= 8'h59;
+                  end
+                8'h?0:
+                  begin
+                    min_reg <= {(min_reg[7:4]-4'h1), 4'h9};
+                  end
+                default:
+                  begin
+                    min_reg <= min_reg - 8'h01;
+                  end
+              endcase
+            end
+
+            //hours
+            if((sec_reg == 8'h00) & (min_reg == 8'h00))
+            begin
+              casex(hour_reg)
+                8'h00:
+                  begin
+                    hour_reg <= 8'h00;
+                  end
+                8'h10:
+                  begin
+                    hour_reg <= 8'h09;
+                  end
+                8'h20: 
+                  begin
+                    hour_reg <= 8'h19;
+                  end
+                default:
+                  begin
+                    hour_reg <= hour_reg - 8'h01;
+                  end
+              endcase
+            end
+        end
+    end
+
+endmodule
 
 module datemodule(input clk, input [7:0] hour_in, input [23:0] date_in, input [2:0] weekday_in, input [1:0] date_mode, output [23:0] date_out, output [2:0] weekday_out);
 
@@ -223,7 +305,7 @@ module settime (
     always @(posedge clk) begin
         if (set_mode == 2'b01) begin
             if (!issetpressednow) begin // Check if setting mode is just entered
-                state <= HOUR;
+                state <= HOUR; 
                 issetpressednow <= 1; // Reset issetpressednow flag
                 hour1 <= 4'b0;
                 hour2 <= 4'b0;
@@ -381,9 +463,11 @@ endmodule //clocktime
 
 module formattime (
   input wire [1:0] clk_mode,     // Clock mode (2-bit)
+  input wire [1:0] timer_mode,  // timer_mode
   input wire setampm,  // Time format toggle signal
   input wire [23:0] clock_time, // Current clock time (24-bit)
   input wire [23:0] alarm_time, // Alarm time (24-bit)
+  input wire [23:0] timer_time, // Timer time (24-bit)
   output [23:0] bcd_time,   // Converted time output (24-bit)
   output ampm);
 
@@ -392,14 +476,17 @@ module formattime (
   wire [5:0] hour12;
   wire issetampm = 0;
 
-  assign hour24 = (clk_mode == 2'b11) ? alarm_time : clock_time;
+  //timer_mode should be 0 for the rest of the module to work the same
+  assign hour24 = (timer_mode == 2'b10 || timer_mode == 2'b01) ? timer_time : ((clk_mode == 2'b11) ? alarm_time : clock_time);
   assign hour24Sub = hour24 - ((hour24[21:17]==5'b10000) ? 6'h18 : 6'h12); //takes care of the 9,10 bug in pm mode
   
-  assign ampm = (hour24[23:16] > 8'h11); //1-->pm, 0-->am
+  //changed ampm conditions to display timer counting backwards
+  assign ampm = (hour24[23:16] > 8'h11 && timer_mode == 0 && clk_mode == 0); //1-->pm, 0-->am
   assign hour12 = ((hour24 == 24'h0) | (hour24 == 24'h120000)) ? 6'h12 : ((ampm) ? hour24Sub : hour24[21:16]);
 
   assign issetampm = (setampm == 1) ? ~issetampm : issetampm;
-  assign bcd_time = (issetampm) ? {2'b00, hour12, hour24[15:0]} : hour24;
+  //changed ampm conditions to display timer counting backwards
+  assign bcd_time = (issetampm && timer_mode == 0 && clk_mode == 0) ? {2'b00, hour12, hour24[15:0]} : hour24;
 
 endmodule //formattime
 
